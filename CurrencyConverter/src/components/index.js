e1c5f217eabef9b37b9cbf64a000ef5a80e0444c
Subodhini Aggarwal <138358378+Subodhini0906@users.noreply.github.com>
2024-11-20 22:41:19 +0530

    Update index.js

--- CurrencyConverter/src/components/index.js
+++ CurrencyConverter/src/components/index.js
@@ -1,2 +1,3 @@
+//This file we genrally create in big code bases here we import every components and then we will export each components from here
 import InputBox from "./InputBox";
 export {InputBox}
